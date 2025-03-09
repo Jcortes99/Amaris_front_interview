803bd240c41e56c5c73b900fa1e6c33bea09e586
Jcortes99 <jcortesro99@gmail.com>
2025-03-09 12:21:39 -0500

    Update CSS files for better view and show the grid in table; Update data-fetcher component: showing better mesage about errors.

--- src/app/data-fetcher.component.ts
+++ src/app/data-fetcher.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { json } from 'node:stream/consumers';
 
 
 @Component({
@@ -17,6 +18,8 @@ export class DataFetcherComponent {
     dataKeys: string[] = [];
     listData: string[] = [];
     isArray: boolean = false;
+    sortColumn: string | null = null;
+    sortDirection: 'asc' | 'desc' = 'asc';
 
     constructor(private http: HttpClient) {}
 
@@ -43,7 +46,24 @@ export class DataFetcherComponent {
                     this.dataKeys = Object.keys(response);
                 }
             },
-            error => console.error('Error fetching data:', error)
+            error => {
+                console.log(error)
+                let errorMessage = 'Unknown Error';
+                try{
+                    const match = error.error.text.match(/"message":\s*"([^"]+)"/);
+                    if (match && match[1]) {
+                        errorMessage = match[1];
+                        if (errorMessage == "Too Many Attempts."){
+                            errorMessage = "Too Many Attempts"
+                        } 
+                    } else {
+                        errorMessage = error.error.message || 'Unknown Error';
+                    }
+                } catch(e){
+                    errorMessage = error.error.message;
+                }
+                alert(`Error!! : ${errorMessage}, Wait 30 seconds`);
+            }
         );
     }
 
@@ -51,6 +71,28 @@ export class DataFetcherComponent {
         const newObj = { ...obj };
         delete newObj.profile_image;
         return newObj;
+    }
+
+    sortData(column: string) {
+        if (this.sortColumn === column) {
+            this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            this.sortColumn = column;
+            this.sortDirection = 'asc';
+        }
+
+        this.data.sort((a: any, b: any) => {
+            const valueA = a[column];
+            const valueB = b[column];
+
+            if (typeof valueA === 'number' && typeof valueB === 'number') {
+                return this.sortDirection === 'asc' ? valueA - valueB : valueB - valueA;
+            } else {
+                return this.sortDirection === 'asc' 
+                    ? valueA.toString().localeCompare(valueB.toString()) 
+                    : valueB.toString().localeCompare(valueA.toString());
+            }
+        });
     }
 
 }
